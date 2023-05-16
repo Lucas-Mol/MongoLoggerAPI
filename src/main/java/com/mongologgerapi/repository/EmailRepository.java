6cc78da380cdb95dae865064bdd3c9f2b72acccf
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-15 22:45:16 -0300

    fix(*Repository): fixing findAll methods to search by only Repository class

--- src/main/java/com/mongologgerapi/repository/EmailRepository.java
+++ src/main/java/com/mongologgerapi/repository/EmailRepository.java
@@ -4,23 +4,31 @@ import com.mongologgerapi.domain.model.Email;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmailRepository extends MongoRepository<Email, String> {
 
+    @Query("{ '_class': 'email'}")
     Page<Email> findAll(Pageable pageable);
 
+    @Query("{ '_class': 'email', 'userId': ?0 }")
     Page<Email> findAllByUserId(Pageable pageable, Long userId);
 
+    @Query("{ '_class': 'email', 'sender': ?0 }")
     Page<Email> findAllBySender(Pageable pageable, String sender);
 
+    @Query("{ '_class': 'email', 'recipient': ?0 }")
     Page<Email> findAllByRecipient(Pageable pageable, String recipient);
 
+    @Query("{ '_class': 'email', 'userId': ?0 , 'recipient':  ?1}")
     Page<Email> findAllByUserIdAndRecipient(Pageable pageable, Long userId, String recipient);
 
+    @Query("{ '_class': 'email', 'sender': ?0 , 'recipient':  ?1}")
     Page<Email> findAllBySenderAndRecipient(Pageable pageable, String sender, String recipient);
 
+    @Query("{ '_class': 'email', 'userId': ?0 , 'sender':  ?1, 'recipient':  ?2}")
     Page<Email> findAllByUserIdAndSenderAndRecipient(Pageable pageable, Long userId, String sender, String recipient);
 
 }
--- src/main/java/com/mongologgerapi/repository/PostRepository.java
+++ src/main/java/com/mongologgerapi/repository/PostRepository.java
@@ -4,18 +4,23 @@ import com.mongologgerapi.domain.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ '_class': 'post'}")
     Page<Post> findAll(Pageable pageable);
 
+    @Query("{ '_class': 'post', 'userId': ?0 }")
     Page<Post> findAllByUserId(Pageable pageable, Long userId);
 
+    @Query("{ '_class': 'post', 'postId': ?0 }")
     Page<Post> findAllByPostId(Pageable pageable, Long postId);
 
+    @Query("{ '_class': 'post', 'userId': ?0 , 'postId':  ?1}")
     Page<Post> findAllByUserIdAndPostId(Pageable pageable, Long userId, Long postId);
 
 }
